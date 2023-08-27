d5136e243b338ea18a06d604fd02711130a7fe54
JayaPK21 <80857811+JayaPK21@users.noreply.github.com>
2023-08-27 10:31:29 +0100

    comments out the database code for now

--- classes/score.py
+++ classes/score.py
@@ -11,8 +11,8 @@ class Score:
         self.player_name = Player 
         self.sc_date = date.today()
         self.value = 0
-        self.connection = mysql.connector.connect('snake_db')
-        self.cursor = self.connection.cursor()
+        # self.connection = mysql.connector.connect('snake_db')
+        # self.cursor = self.connection.cursor()
 
     def increase(self):
         self.value += 1
@@ -28,10 +28,10 @@ class Score:
     #     self.cursor.execute(""" 
     #                """)
 
-    def update_new_player_to_database(self, score_id=-1, player_id =-1,):
-        self.cursor.execute(""" INSERT INTO score (
-        score_id, player_id, sc_date, score)VALUES 
-        ({}, {} ,'{}', {}""").format(score_id=-1, player_id =-1, sc_date = date.today(), self.value) # the score variable needed here
+    # def update_new_player_to_database(self, score_id=-1, player_id =-1,):
+    #     self.cursor.execute(""" INSERT INTO score (
+    #     score_id, player_id, sc_date, score)VALUES 
+    #     ({}, {} ,'{}', {}""").format(score_id=-1, player_id =-1, sc_date = date.today(), self.value) # the score variable needed here
 
  
  
